d06108a5ee9d61729d9839405ef43dfb27db8ec9
ni3sama <36023560+ni3sama@users.noreply.github.com>
2021-07-11 22:01:25 +0530

    Update Applicant.js

--- src/Applicant/Applicant.js
+++ src/Applicant/Applicant.js
@@ -19,7 +19,7 @@ const getJob = () => {
 }
   return (
     <div className="formContainer">
-    <p>Disclaimer: This definitely works, if you see no data here, that means I am out of query request or reached the limit of the database plan, since this is a free hosted server.</p>
+    <p>Disclaimer: This definitely works, if you see no data here, that means I am out of query request or reached the limit of the database plan, since this is a free hosted server. Wait 1 hour or ask me to reset the server.</p>
         <div className="jobList">
         <h1>Available Jobs!</h1>
          {getJob()}
